c48ab287d70807505a5858edf2fe7231ec0de69d
Guilherme <gui.ballestrim@gmail.com>
2025-10-07 10:25:10 -0300

    Criação da classe e da função

---
+++ classes.py
@@ -0,0 +1,34 @@
+import os
+import datetime
+
+
+class GerarRelatorio:
+    def Gerar(self, *paragrafos, **caracteristicas):
+        corpo = "\n".join(paragrafos)
+
+        if "titulo" in caracteristicas:
+            corpo = f"{caracteristicas['titulo']}\n\n{corpo}"
+
+        if "rodape" in caracteristicas:
+            corpo = f"{corpo}\n\n{caracteristicas['rodape']}"
+
+        if "metadados" in caracteristicas:
+            corpo += f"\n\nMetadados: "
+            for chave, valor in caracteristicas["metadados"].items():
+                corpo += f"\n{chave} - {valor}"
+
+        return corpo
+
+
+relatorio = GerarRelatorio()
+texto = relatorio.Gerar(
+    "Parágrafo 1",
+    "Parágrafo 2",
+    titulo="Título",
+    rodape="Rodapé",
+    metadados={
+        "Autor": os.environ.get("USERNAME"),
+        "Data": datetime.datetime.today().strftime("%d/%m/%Y"),
+        "Versão": 1.0,
+    },
+)
